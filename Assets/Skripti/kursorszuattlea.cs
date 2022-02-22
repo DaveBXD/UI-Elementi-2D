5819e7a700030324a8307a04329f4b9624a5a6da
Barons <davis.barons2015@gmail.com>
2022-02-22 15:26:01 +0200

    Beigas

---
+++ Assets/Skripti/kursorszuattlea.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kursorszuattlea : MonoBehaviour {
+	public AudioSource skanasavots;
+	public AudioClip skanaKoAtskanot;
+
+	public void uzbiditsUzAttela(){
+		skanasavots.PlayOneShot (skanaKoAtskanot);
+		print ("skan");
+	}
+
+	public void nobiditsNoAttela(){
+		skanasavots.Stop ();
+		print ("Beidz skanet");
+	}
+}
